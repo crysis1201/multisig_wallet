4c7efe5c486c9fccd8047bb04ef1ef162f6394dc
Eazhilamuthan <eazhilamuthan12@gmail.com>
2022-06-24 20:08:40 +0530

    added remaining functions

--- programs/multisig-wallet/src/lib.rs
+++ programs/multisig-wallet/src/lib.rs
@@ -1,7 +1,11 @@
 use anchor_lang::prelude::*;
 use anchor_lang::accounts::program_account::ProgramAccount;
+use anchor_lang::solana_program;
+use anchor_lang::solana_program::instruction::Instruction;
+use std::convert::Into;
+use std::ops::Deref;
 
-declare_id!("75ed4cXcxNrViHDKhH3apxduALNJ5RDHRQP3kPqf86jd");
+declare_id!("2qifYLbMLjVKhXH95Q5zCaKFfx4YhZDDeuCHJ9QhpJHX");
 
 #[program]
 pub mod multisig_wallet {
@@ -15,13 +19,186 @@ pub mod multisig_wallet {
         multisig.owner_set_seqno = 0;
         Ok(())
     }
+
+    pub fn create_transaction(
+        ctx: Context<CreateTransaction>,
+        pid: Pubkey,
+        accs: Vec<TransactionAccount>,
+        data: Vec<u8>,
+    ) -> Result<()> {
+        let owner_index = ctx
+            .accounts
+            .multisig
+            .owners
+            .iter()
+            .position(|a| a == ctx.accounts.proposer.key)
+            .ok_or(ErrorCode::InvalidOwner)?;
+
+        let mut signers = Vec::new();
+        signers.resize(ctx.accounts.multisig.owners.len(), false);
+        signers[owner_index] = true;
+
+        let tx = &mut ctx.accounts.transaction;
+        tx.program_id = pid;
+        tx.accounts = accs;
+        tx.data = data;
+        tx.signers = signers;
+        tx.multisig = ctx.accounts.multisig.key();
+        tx.did_execute = false;
+        tx.did_reject = false;
+        tx.owner_set_seqno = ctx.accounts.multisig.owner_set_seqno;
+
+        Ok(())
+    }
+
+    // Set owners and threshold at once.
+    pub fn set_owners_and_change_threshold(ctx: Context<Auth>, owners: Vec<Pubkey>, threshold: u64) -> Result<()> {
+        require!(threshold > 0, InvalidThreshold);
+
+        if threshold > owners.len() as u64 {
+            return Err(ErrorCode::InvalidThreshold.into());
+        }
+
+        let multisig = &mut ctx.accounts.multisig;
+
+        multisig.threshold = threshold;
+        multisig.owners = owners;
+        multisig.owner_set_seqno += 1;
+        Ok(())
+    }
+
+    pub fn approve(ctx: Context<ApproveReject>) -> Result<()> {
+        let owner_index = ctx
+            .accounts
+            .multisig
+            .owners
+            .iter()
+            .position(|a| a == ctx.accounts.owner.key)
+            .ok_or(ErrorCode::InvalidOwner)?;
+
+        ctx.accounts.transaction.signers[owner_index] = true;
+
+        Ok(())
+    }
+
+    pub fn reject(ctx: Context<ApproveReject>) -> Result<()> {
+        let _owner_index = ctx
+            .accounts
+            .multisig
+            .owners
+            .iter()
+            .position(|a| a == ctx.accounts.owner.key)
+            .ok_or(ErrorCode::InvalidOwner)?;
+
+        let sig_count = ctx 
+            .accounts
+            .transaction
+            .signers
+            .iter()
+            .filter(|&did_sign| *did_sign)
+            .count() as u64;
+        if sig_count < ctx.accounts.multisig.threshold {
+            ctx.accounts.transaction.did_reject = true
+        }
+
+        Ok(())
+    }
+
+    pub fn execute_transaction(ctx: Context<ExecuteTransaction>) -> Result<()> {
+        // Has this been executed already?
+        if ctx.accounts.transaction.did_execute {
+            return Err(ErrorCode::AlreadyExecuted.into());
+        }
+
+        // Has this been Rejected already?
+        if ctx.accounts.transaction.did_reject {
+            return Err(ErrorCode::TransactionRejected.into());
+        }
+
+        // Do we have enough signers.
+        let sig_count = ctx
+            .accounts
+            .transaction
+            .signers
+            .iter()
+            .filter(|&did_sign| *did_sign)
+            .count() as u64;
+        if sig_count < ctx.accounts.multisig.threshold {
+            return Err(ErrorCode::NotEnoughSigners.into());
+        }
+
+        // Execute the transaction signed by the multisig.
+        let mut ix: Instruction = (*ctx.accounts.transaction).deref().into();
+        ix.accounts = ix
+            .accounts
+            .iter()
+            .map(|acc| {
+                let mut acc = acc.clone();
+                if &acc.pubkey == ctx.accounts.multisig_signer.key {
+                    acc.is_signer = true;
+                }
+                acc
+            })
+            .collect();
+        let multisig_key = ctx.accounts.multisig.key();
+        let seeds = &[multisig_key.as_ref(), &[ctx.accounts.multisig.nonce]];
+        let signer = &[&seeds[..]];
+        let accounts = ctx.remaining_accounts;
+        solana_program::program::invoke_signed(&ix, accounts, signer)?;
+
+        // Burn the transaction to ensure one time use.
+        ctx.accounts.transaction.did_execute = true;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct CreateAccount<'info> {
-    #[account(zero)]
-    multisig: ProgramAccount<'info, Multisig>,
-    rent: Sysvar<'info, Rent>
+    #[account(zero, signer)]
+    multisig: Box<Account<'info, Multisig>>,
+}
+
+#[derive(Accounts)]
+pub struct CreateTransaction<'info> {
+    multisig: Box<Account<'info, Multisig>>,
+    #[account(zero, signer)]
+    transaction: Box<Account<'info, Transaction>>,
+    proposer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ApproveReject<'info> {
+    #[account(constraint = multisig.owner_set_seqno == transaction.owner_set_seqno)]
+    multisig: Box<Account<'info, Multisig>>,
+    #[account(mut, has_one = multisig)]
+    transaction: Box<Account<'info, Transaction>>,
+    owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Auth<'info> {
+    #[account(mut)]
+    multisig: Box<Account<'info, Multisig>>,
+    #[account(
+        seeds = [multisig.key().as_ref()],
+        bump = multisig.nonce,
+    )]
+    multisig_signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ExecuteTransaction<'info> {
+    #[account(constraint = multisig.owner_set_seqno == transaction.owner_set_seqno)]
+    multisig: Box<Account<'info, Multisig>>,
+    /// CHECK: multisig_signer is a PDA program signer
+    #[account(
+        seeds = [multisig.key().as_ref()],
+        bump = multisig.nonce,
+    )]
+    multisig_signer: UncheckedAccount<'info>,
+    #[account(mut, has_one = multisig)]
+    transaction: Box<Account<'info, Transaction>>,
 }
 
 #[account]
@@ -30,4 +207,76 @@ pub struct Multisig {
     pub threshold: u64,
     pub nonce: u8,
     pub owner_set_seqno: u32,
+}
+
+#[account]
+pub struct Transaction {
+    pub multisig: Pubkey,
+    pub program_id: Pubkey,
+    pub accounts: Vec<TransactionAccount>,
+    pub data: Vec<u8>,
+    pub signers: Vec<bool>,
+    pub did_execute: bool,
+    pub did_reject: bool,
+    pub owner_set_seqno: u32,
+}
+
+impl From<&Transaction> for Instruction {
+    fn from(tx: &Transaction) -> Instruction {
+        Instruction {
+            program_id: tx.program_id,
+            accounts: tx.accounts.iter().map(Into::into).collect(),
+            data: tx.data.clone(),
+        }
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct TransactionAccount {
+    pub pubkey: Pubkey,
+    pub is_signer: bool,
+    pub is_writable: bool,
+}
+
+impl From<&TransactionAccount> for AccountMeta {
+    fn from(account: &TransactionAccount) -> AccountMeta {
+        match account.is_writable {
+            false => AccountMeta::new_readonly(account.pubkey, account.is_signer),
+            true => AccountMeta::new(account.pubkey, account.is_signer),
+        }
+    }
+}
+
+impl From<&AccountMeta> for TransactionAccount {
+    fn from(account_meta: &AccountMeta) -> TransactionAccount {
+        TransactionAccount {
+            pubkey: account_meta.pubkey,
+            is_signer: account_meta.is_signer,
+            is_writable: account_meta.is_writable,
+        }
+    }
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The given owner is not part of this multisig.")]
+    InvalidOwner,
+    #[msg("Owners length must be non zero.")]
+    InvalidOwnersLen,
+    #[msg("Not enough owners signed this transaction.")]
+    NotEnoughSigners,
+    #[msg("Cannot delete a transaction that has been signed by an owner.")]
+    TransactionAlreadySigned,
+    #[msg("Transaction Rejected")]
+    TransactionRejected,
+    #[msg("Overflow when adding.")]
+    Overflow,
+    #[msg("Cannot delete a transaction the owner did not create.")]
+    UnableToDelete,
+    #[msg("The given transaction has already been executed.")]
+    AlreadyExecuted,
+    #[msg("Threshold must be less than or equal to the number of owners.")]
+    InvalidThreshold,
+    #[msg("Owners must be unique")]
+    UniqueOwners,
 }
--- tests/multisig-wallet.ts
+++ tests/multisig-wallet.ts
@@ -1,21 +1,24 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { MultisigWallet } from '../targeT/types/multisig_wallet'
+import { MultisigWallet } from '../target/types/multisig_wallet'
 const assert = require("assert");
 
 describe("multisig-wallet", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.getProvider());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
+  
 
   const program = anchor.workspace.MultisigWallet as Program<MultisigWallet>
 
   it("Create Multisig", async () => {
     // Add your test here.
     const multisig = anchor.web3.Keypair.generate();
-    const[, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+    const[multisigSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
       [multisig.publicKey.toBuffer()],
       program.programId
     );
+    console.log(multisigSigner)
     const multisigSize = 200;
 
     const ownerA = anchor.web3.Keypair.generate();
@@ -28,7 +31,6 @@ describe("multisig-wallet", () => {
     const tx = await program.rpc.createAccount(owners, new anchor.BN(threshold), nonce, {
       accounts: {
         multisig: multisig.publicKey,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY
       },
       instructions: [
         await program.account.multisig.createInstruction(
@@ -46,5 +48,225 @@ describe("multisig-wallet", () => {
     assert.ok(multisigAccount.threshold.eq(new anchor.BN(2)));
     assert.deepStrictEqual(multisigAccount.owners, owners);
     assert.ok(multisigAccount.ownerSetSeqno === 0);
+
+    const pid = program.programId;
+    const accounts = [
+      {
+        pubkey: multisig.publicKey,
+        isWritable: true,
+        isSigner: false,
+      },
+      {
+        pubkey: multisigSigner,
+        isWritable: false,
+        isSigner: true,
+      },
+    ];
+    //const data = anchor.web3.SystemProgram.transfer({
+    //  fromPubkey: multisigSigner,
+    //  toPubkey: ownerA.publicKey,
+    //  lamports: new anchor.BN(1000000000),
+    //}).data
+
+    //create transaction account 
+    const transaction = anchor.web3.Keypair.generate();
+    const txSize = 1000; // Big enough, cuz I'm lazy.
+    const newOwners = [ownerA.publicKey, ownerB.publicKey];
+    const data = program.coder.instruction.encode("set_owners_and_change_threshold", {
+      owners: newOwners,
+      threshold: new anchor.BN(2)
+    });
+
+    //const transaction = anchor.web3.Keypair.generate();
+    //const txSize = 1000; // Big enough, cuz I'm lazy.
+    await program.rpc.createTransaction(pid, accounts, data, {
+      accounts: {
+        multisig: multisig.publicKey,
+        transaction: transaction.publicKey,
+        proposer: ownerA.publicKey,
+        //rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      },
+      instructions: [
+        await program.account.transaction.createInstruction(
+          transaction,
+          txSize
+        ),
+      ],
+      signers: [transaction, ownerA],
+    });
+
+    const txAccount = await program.account.transaction.fetch(
+      transaction.publicKey
+    );
+
+    console.log(txAccount)
+
+    assert.ok(txAccount.programId.equals(pid));
+    assert.deepStrictEqual(txAccount.accounts, accounts);
+    assert.deepStrictEqual(txAccount.data, data);
+    assert.ok(txAccount.multisig.equals(multisig.publicKey));
+    assert.deepStrictEqual(txAccount.didExecute, false);
+
+     await program.rpc.approve({
+      accounts: {
+        multisig: multisig.publicKey,
+        transaction: transaction.publicKey,
+        owner: ownerB.publicKey,
+      },
+      signers: [ownerB],
+    });
+
+    const txAccout = await program.account.transaction.fetch(
+      transaction.publicKey
+    );
+
+    assert.deepStrictEqual(txAccout.didReject, false);
+
+    // Now that we've reached the threshold, send the transactoin.
+    await program.rpc.executeTransaction({
+      accounts: {
+        multisig: multisig.publicKey,
+        multisigSigner,
+        transaction: transaction.publicKey,
+      },
+      remainingAccounts: program.instruction.setOwnersAndChangeThreshold
+        .accounts({
+          multisig: multisig.publicKey,
+          multisigSigner,
+        })
+        // Change the signer status on the vendor signer since it's signed by the program, not the client.
+        .map((meta) =>
+          meta.pubkey.equals(multisigSigner)
+            ? { ...meta, isSigner: false }
+            : meta
+        )
+        .concat({
+          pubkey: program.programId,
+          isWritable: false,
+          isSigner: false,
+        }),
+    });
+
+    multisigAccount = await program.account.multisig.fetch(multisig.publicKey);
+    const result = await program.account.transaction.fetch(transaction.publicKey);
+    console.log("final", result)
+
+    assert.strictEqual(multisigAccount.nonce, nonce);
+    assert.ok(multisigAccount.threshold.eq(new anchor.BN(2)));
+    assert.deepStrictEqual(multisigAccount.owners, newOwners);
+    assert.ok(multisigAccount.ownerSetSeqno === 1);
   });
-})
+
+  it("Reject Function", async () => {
+    // Add your test here.
+    const multisig = anchor.web3.Keypair.generate();
+    const[multisigSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [multisig.publicKey.toBuffer()],
+      program.programId
+    );
+    console.log(multisigSigner)
+    const multisigSize = 200;
+
+    const ownerA = anchor.web3.Keypair.generate();
+    const ownerB = anchor.web3.Keypair.generate();
+    const ownerC = anchor.web3.Keypair.generate();
+
+    const owners = [ownerA.publicKey, ownerB.publicKey, ownerC.publicKey]
+
+    const threshold = 2
+    const tx = await program.rpc.createAccount(owners, new anchor.BN(threshold), nonce, {
+      accounts: {
+        multisig: multisig.publicKey,
+      },
+      instructions: [
+        await program.account.multisig.createInstruction(
+          multisig,
+          multisigSize
+        )
+      ],
+      signers: [multisig]
+    })
+    console.log(await tx)
+
+    let multisigAccount = await program.account.multisig.fetch(multisig.publicKey);
+    console.log({multisigAccount});
+    assert.strictEqual(multisigAccount.nonce, nonce);
+    assert.ok(multisigAccount.threshold.eq(new anchor.BN(2)));
+    assert.deepStrictEqual(multisigAccount.owners, owners);
+    assert.ok(multisigAccount.ownerSetSeqno === 0);
+
+    const pid = program.programId;
+    const accounts = [
+      {
+        pubkey: multisig.publicKey,
+        isWritable: true,
+        isSigner: false,
+      },
+      {
+        pubkey: multisigSigner,
+        isWritable: false,
+        isSigner: true,
+      },
+    ];
+    const newOwners = [ownerA.publicKey, ownerB.publicKey];
+    const data = program.coder.instruction.encode("set_owners_and_change_threshold", {
+      owners: newOwners,
+      threshold: new anchor.BN(2)
+    });
+
+    const transaction = anchor.web3.Keypair.generate();
+    const txSize = 1000;
+    await program.rpc.createTransaction(pid, accounts, data, {
+      accounts: {
+        multisig: multisig.publicKey,
+        transaction: transaction.publicKey,
+        proposer: ownerA.publicKey,
+        //rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      },
+      instructions: [
+        await program.account.transaction.createInstruction(
+          transaction,
+          txSize
+        ),
+      ],
+      signers: [transaction, ownerA],
+    });
+
+    const txAccount = await program.account.transaction.fetch(
+      transaction.publicKey
+    );
+
+    console.log(txAccount)
+
+    assert.ok(txAccount.programId.equals(pid));
+    assert.deepStrictEqual(txAccount.accounts, accounts);
+    assert.deepStrictEqual(txAccount.data, data);
+    assert.ok(txAccount.multisig.equals(multisig.publicKey));
+    assert.deepStrictEqual(txAccount.didExecute, false);
+    assert.ok(txAccount.ownerSetSeqno === 0);
+
+     await program.rpc.reject({
+      accounts: {
+        multisig: multisig.publicKey,
+        transaction: transaction.publicKey,
+        owner: ownerB.publicKey,
+      },
+      signers: [ownerB],
+    });
+
+    const txAccout = await program.account.transaction.fetch(
+      transaction.publicKey
+    );
+
+    assert.deepStrictEqual(txAccout.didReject, true);
+    })
+
+//    async function transfer(from, to) {
+//      const instructions = [anchor.web3.SystemProgram.transfer({
+//      fromPubkey: from,
+//      toPubkey: to,
+//      lamports: new anchor.BN(3000000000),
+//    }),
+//    ]
+//  }
+});
